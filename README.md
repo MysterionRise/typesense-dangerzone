a84715c0c20c5f56a59ad799ba2aa97f8b8158ef
Konstantin Perikov <pericov.ko@gmail.com>
2023-06-25 23:30:24 +0100

    feat: add typesense docker compose file and create collection example

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # typesense-dangerzone
 Repo dedicated to trying out Typesets - open-source alternative to Algolia and Elasticsearch
+
+## How To Run
+
+1. Install Docker
+2. Run `docker-compose up`
+3. Go to `localhost:8080` to see the Typesense dashboard
---
+++ create_collection.py
@@ -0,0 +1,23 @@
+from typesense import client
+
+schema = {
+  'name': 'companies',
+  'fields': [
+    {
+      'name'  :  'company_name',
+      'type'  :  'string'
+    },
+    {
+      'name'  :  'num_employees',
+      'type'  :  'int32'
+    },
+    {
+      'name'  :  'country',
+      'type'  :  'string',
+      'facet' :  True
+    }
+  ],
+  'default_sorting_field': 'num_employees'
+}
+
+client.collections.create(schema)

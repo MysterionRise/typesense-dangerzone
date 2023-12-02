985dce5ea5c710410927fb24531bf265c0e06a47
Konstantin Perikov <pericov.ko@gmail.com>
2023-12-02 15:28:41 +0000

    chore: update dependencies

--- create_collection.py
+++ create_collection.py
@@ -1,23 +1,13 @@
 from typesense import client
 
 schema = {
-  'name': 'companies',
-  'fields': [
-    {
-      'name'  :  'company_name',
-      'type'  :  'string'
-    },
-    {
-      'name'  :  'num_employees',
-      'type'  :  'int32'
-    },
-    {
-      'name'  :  'country',
-      'type'  :  'string',
-      'facet' :  True
-    }
-  ],
-  'default_sorting_field': 'num_employees'
+    "name": "companies",
+    "fields": [
+        {"name": "company_name", "type": "string"},
+        {"name": "num_employees", "type": "int32"},
+        {"name": "country", "type": "string", "facet": True},
+    ],
+    "default_sorting_field": "num_employees",
 }
 
 client.collections.create(schema)
